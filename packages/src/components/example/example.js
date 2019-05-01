2cfeaee1b7bc798f881b8b139333648f3ce6fccb
Łukasz <lukasz.rdzanek@protonmail.com>
2019-05-01 23:18:49 +0200

    [structure] added project structure with temporary file examples

---
+++ packages/src/components/example/example.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import './example.css';
+
+export default class Example extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
