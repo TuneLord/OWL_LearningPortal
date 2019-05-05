37a1b5fec8b9084f4dbcfbd03cc35d36fa47b45d
Gustaw <tanski.gustaw@gmail.com>
2019-05-06 00:55:28 +0200

    Commented and cleaned

--- packages/src/components/TextEditor/sources/ImageSource.jsx
+++ packages/src/components/TextEditor/sources/ImageSource.jsx
@@ -28,7 +28,6 @@ export default class ImageSource extends Component {
 
   componentDidMount()
   {
-    console.log(this.props);
     this.props.entityType.UIHandler(this.callback.bind(this));
   }
 
