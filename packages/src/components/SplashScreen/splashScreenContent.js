4d56eaa2f04c9e1f9dcd08e14ff3afd340386682
Łukasz <lukasz.rdzanek@protonmail.com>
2019-05-05 18:25:25 +0200

    small content fixes

--- packages/src/components/SplashScreen/splashScreenContent.js
+++ packages/src/components/SplashScreen/splashScreenContent.js
@@ -6,7 +6,7 @@ export const SplashScreenContent = () => {
       <section className='splashScreenContent'>
         <div>
           <h1><strong>Nauczanie</strong> w internecie jest proste i skuteczne!</h1>
-          <p>Nauczanie w internecie nigdy nie było łatwiejsze. Skomponuj własne materiały i dziel się nimi z tylko chcesz.</p>
+          <p>Nauczanie w internecie nigdy nie było łatwiejsze. Skomponuj własne materiały i dziel się nimi z kim tylko chcesz.</p>
           <a className="menu-item" href="/register">Załóż konto</a>
         </div>
       </section>
