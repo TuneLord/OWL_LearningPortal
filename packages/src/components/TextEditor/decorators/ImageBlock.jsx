4597ba236ae662110d590938fb596a893a683128
Gustaw <tanski.gustaw@gmail.com>
2019-05-07 21:59:49 +0200

    Image entity almost done
    Only some styles are needed. Also code isn't cleaned and there are not comments everywhere they should be

--- packages/src/components/TextEditor/decorators/ImageBlock.jsx
+++ packages/src/components/TextEditor/decorators/ImageBlock.jsx
@@ -1,11 +1,26 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import unionClassNames from "union-class-names";
 
 export default class ImageBlock extends Component {
+
   render() {
-    const { blockProps } = this.props
+    const { block, className, theme, ...otherProps } = this.props;
+    const {
+      blockProps,
+      customStyleMap,
+      customStyleFn,
+      decorator,
+      forceSelection,
+      offsetKey,
+      selection,
+      tree,
+      contentState,
+      blockStyleFn,
+      ...elementProps
+    } = otherProps;
     const { entity } = blockProps
     const { src, alt } = entity.getData()
-
-    return <img className="ImageBlock" src={src} alt={alt} width="256" />
+    const newClassName = unionClassNames("editor-image-block", className)
+    return <img {...elementProps} className={newClassName} src={src} alt={alt} width="256" />
   }
 }
