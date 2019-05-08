32d66b9541faa12faf69244a6c9cf49068792af6
Gustaw <tanski.gustaw@gmail.com>
2019-05-08 08:56:52 +0200

    Commented and cleaned

--- packages/src/components/TextEditor/decorators/ImageBlock.jsx
+++ packages/src/components/TextEditor/decorators/ImageBlock.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import unionClassNames from "union-class-names";
 
-export default class ImageBlock extends Component {
-
-  render() {
+export default class ImageBlock extends React.Component 
+{
+  render() 
+  {
+    // Filtering the props to get img element's props and other useful props
     const { block, className, theme, ...otherProps } = this.props;
     const {
       blockProps,
@@ -18,9 +20,9 @@ export default class ImageBlock extends Component {
       blockStyleFn,
       ...elementProps
     } = otherProps;
-    const { entity } = blockProps
-    const { src, alt } = entity.getData()
-    const newClassName = unionClassNames("editor-image-block", className)
-    return <img {...elementProps} className={newClassName} src={src} alt={alt} width="256" />
+    const { entity } = blockProps;
+    const { src, alt } = entity.getData();
+    const newClassName = unionClassNames("editor-image-block", className);
+    return <img {...elementProps} className={newClassName} src={src} alt={alt} width="256" />''
   }
 }
--- packages/src/components/TextEditor/sources/linkSource.jsx
+++ packages/src/components/TextEditor/sources/linkSource.jsx
@@ -1,28 +1,32 @@
-import { Component } from "react";
+import React from "react";
 import { Modifier, EditorState } from "draft-js";
 
-export default class ImageSource extends Component {
+
+// This source component is used to creating link entity. Read draft.js documentation or ask 
+// me to understand what is going on below
+export default class ImageSource extends React.Component 
+{
   callback(src) 
   {
-    const { editorState, entityType, onComplete } = this.props
-    if (src) {
-        const content = editorState.getCurrentContent()
-        const contentWithEntity = content.createEntity(
-            entityType.type,
-            "MUTABLE",
-            { src },
-            )
-            const entityKey = contentWithEntity.getLastCreatedEntityKey();
-            const contentWithLink = Modifier.applyEntity(
-                contentWithEntity,
-                editorState.getSelection(),
-                entityKey,
-            )
-            const nextState = EditorState.push(editorState, contentWithLink);
-      onComplete(nextState)
-    } else {
-      onComplete(editorState)
-    }
+    const { editorState, entityType, onComplete } = this.props;
+    if (src) 
+    {
+      const content = editorState.getCurrentContent();
+      const contentWithEntity = content.createEntity(
+        entityType.type,
+        "MUTABLE",
+        { src },
+        );
+      const entityKey = contentWithEntity.getLastCreatedEntityKey();
+      const contentWithLink = Modifier.applyEntity(
+        contentWithEntity,
+        editorState.getSelection(),
+        entityKey,
+      );
+      const nextState = EditorState.push(editorState, contentWithLink);
+      onComplete(nextState);
+    } 
+    else onComplete(editorState);
   }
 
   componentDidMount()
@@ -30,7 +34,8 @@ export default class ImageSource extends Component {
     this.props.entityType.UIHandler(this.callback.bind(this));
   }
 
-  render() {
+  render() 
+  {
     return null;
   }
 }
