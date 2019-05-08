1a94c6ad6a0912a101579ab4eab4b268795a97d4
AngelikaKuleta <angelcia.kuleta@gmail.com>
2019-05-08 18:29:37 +0200

    sign up/in via Google account

--- packages/src/components/Login/login.js
+++ packages/src/components/Login/login.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './form.css';
 import { connect } from 'react-redux';
-import { loginStatus } from '../../actions/loginStatus'
+import { loginStatus } from '../../actions/loginStatus';
+import { GoogleLogin } from 'react-google-login';
 
 class Login extends React.Component {
     constructor(props) {
@@ -84,22 +85,65 @@ class Login extends React.Component {
         }
     }
 
+    responseGoogle = async (res) => {
+        try {
+            const response = await fetch('/login?googleAuth=true', {
+                method: "post",
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                    "x-auth-token-google": res.tokenId
+                }
+            });
+            if (response.status !== 200) throw response;
+            sessionStorage.setItem("x-auth-token", response.headers.get('x-auth-token'));
+            this.props.loginStatus(true);
+            console.log('Logowanie przebiegło pomyślnie')
+        } catch (err) {
+            console.log(err);
+            if ([404, 400].includes(err.status)) {
+                const error = "Konto nie jest połączone z kontem Google lub usługa chwilowo niedostępna";
+                this.setState({
+                    email: '',
+                    password: '',
+                    isDisable: true,
+                    errors: {
+                        email: error
+                    }
+                });
+            }
+        }  
+    }
+
     render() {
         return (
             <div className='form login'>
                 <h2>Zaloguj się</h2>
                 <form onSubmit={this.onSubmitForm}>
-                    <div>
+                    <div className="form-el">
                         <i className="fas fa-envelope"></i>
                         <input type='email' value={this.state.email} onChange={this.onChangeEmail} placeholder='podaj adres e-mail' />                       
                     </div>
                     {this.state.errors.email && (<div className='error'>{this.state.errors.email}</div>)}
-                    <div>
+                    <div className="form-el">
                         <i className="fas fa-lock"></i>
                         <input type="password" value={this.state.password} onChange={this.onChangePassword} placeholder='podaj hasło' /> 
                     </div>
                     {this.state.errors.password && (<div className='error'>{this.state.errors.password}</div>)}   
                     <input id='submit' type='submit' value='Zaloguj się' disabled={this.state.isDisable}/>
+                    <div className="or"><span>lub</span></div>
+                    <GoogleLogin
+                        clientId="609136166131-5spsmc9vddptv62kfv0i6uttslesqjfq.apps.googleusercontent.com"
+                        render={renderProps => (
+                            <button className='gplus' onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                                <i className="fab fa-google"></i>
+                                <span>Użyj konta Google</span>
+                            </button>
+                        )}
+                        buttonText="Login"
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />      
                 </form>
             </div>
         );
