aebbb4baed8a2a124d353462f1b013fb73224f31
Ania Kujawa <kujawanna@gmail.com>
2019-05-09 13:12:38 +0200

    checklisty + route

--- packages/src/components/MainView/mainView.js
+++ packages/src/components/MainView/mainView.js
@@ -1,30 +1,9 @@
 import React, { Component } from "react";
 import "./mainView.css";
 import { MainViewMenuDesktop } from "./mainViewMenuDesktop";
-import MyChecklists from "./myChecklists";
-import Checklist from "./checklist";
+import MyChecklists from "./myChecklists"
 
 export default class mainView extends Component {
-  state = {
-    data: [{title: 'Pierwszy moduł', isDone: false },{title: 'Drugi moduł', isDone: false} ],
-    isChosen: false
-};
-
-// componentDidMount {
-//     fetch(....., {
-//         method: 'GET',
-         // headers: {
-        //     "x-log-token": localStorage.getItem("token"),
-        // },
-//     }).then(resp => resp.json()
-//     .then(data => setState({data: data})))
-//     .catch(err => console.log(err))
-// }
-
-onLiClick = (e) => {
-    this.setState({isChosen: true })
-    console.log(this.state.isChosen)
-}
   render() {
     const windowWidth = window.innerWidth;
 
@@ -32,8 +11,7 @@ onLiClick = (e) => {
       <section className="mainView">
         {windowWidth > 1025 ? <MainViewMenuDesktop /> : null}
         <div className="mainView__container">
-        {!this.state.isChosen && <MyChecklists data={this.state.data} chosen={this.onLiClick}/> }
-        {this.state.isChosen && <Checklist />}
+      <MyChecklists /> 
         </div>
       </section>
     );
