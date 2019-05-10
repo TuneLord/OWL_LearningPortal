c6c9a4614038f978a7793b398cf67377821d0bed
Ania Kujawa <kujawanna@gmail.com>
2019-05-10 16:32:33 +0200

    teams starter

--- packages/src/components/MainView/mainView.js
+++ packages/src/components/MainView/mainView.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import "./mainView.css";
 import { MainViewMenuDesktop } from "./mainViewMenuDesktop";
 import MyChecklists from "./myChecklists";
+import MyTeams from "./myTeams";
 
 export default class mainView extends Component {
   render() {
     const windowWidth = window.innerWidth;
+    const path = window.location.pathname;
 
     return (
       <section className="mainView">
         {windowWidth > 1025 ? <MainViewMenuDesktop /> : null}
         <div className="mainView__container">
-          <MyChecklists />
+        {path === '/me/myteams' ? <MyTeams /> : null}
+        {path === `/me` ? <MyChecklists /> :null}
         </div>
       </section>
     );
