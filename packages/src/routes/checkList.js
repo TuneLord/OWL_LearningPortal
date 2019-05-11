ba6ddf8582e759229c719690ea85499bb6b969e7
Mateusz Pichniarczyk <mat.pich@interia.pl>
2019-05-11 11:56:55 +0200

    GET /checklist with authorization

--- packages/src/routes/checkList.js
+++ packages/src/routes/checkList.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const checkListExistance = require('../middleware/checkListExistance');
 const Checklist = require('../models/checkListModel');
 const { User } = require('../models/userModel');
 
@@ -13,7 +14,8 @@ router.post('/',auth, async (req, res) => {
         authorId: req.body.authorId, //tutaj id zalogowanego użytkownika tworzącego checklistę wyciągnięte z tokena
         name: req.body.name,
         content: req.body.content,
-        isChecked: req.body.isChecked
+        isChecked: req.body.isChecked,
+        members:[req.body.authorId]
     });
 
     //zapisuję ją do bazy i przechowuję wynik
@@ -27,6 +29,13 @@ router.post('/',auth, async (req, res) => {
     await user.save();
 
     res.status(200).send(user.checkLists);
-})
+});
+
+router.get('/:id', auth, checkListExistance, async (req, res) => {
+    const memberId = req.checklist.members.find(member => member === req.user._id);
+    if (!memberId) return res.status(403).send('Access denied - not a member of a checlist.');
+
+    res.status(200).send(req.checklist);
+});
 
 module.exports = router;
