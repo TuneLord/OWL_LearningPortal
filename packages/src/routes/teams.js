3a939ce24aed7d5c7956327953dcd77e0e0e836c
Marcin <cineqpl@yahoo.com>
2019-05-13 01:34:25 +0200

    [add] post / get / put / delete teams API

--- packages/src/routes/teams.js
+++ packages/src/routes/teams.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const ObjectId = require('mongoose').Types.ObjectId;
 const auth = require('../middleware/auth');
 const Team = require('../models/teamModel');
-
+const { User } = require('../models/userModel');
 const router = express.Router();
 
 router.post('/', auth, async (req, res) => {
 
-    // new team object
+    if(req.user.type !== "mentor") {
+        return res.status(403).send("Access denied - not a mentor.");
+    }
+
     const team = new Team({
-        mentorId: req.body.mentorId, //tutaj id zalogowanego użytkownika tworzącego checklistę wyciągnięte z tokena
+        mentorId: req.user._id,
         name: req.body.name,
-        members: [...req.body.members, req.body.mentorId]
+        members: [...req.body.members, req.user._id]
     });
 
-
-    //zapisuję ją do bazy i przechowuję wynik
     const result = await team.save()
 
     return res.status(200).send(result);
@@ -22,20 +24,80 @@ router.post('/', auth, async (req, res) => {
 
 router.get('/:id', auth, async (req, res) => {
 
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send("Invalid object ID");
+    }
+
     const team = await Team.findById(req.params.id);
-    if (!(team.members.includes(req.body.userId))) return res.status(403).send('Access denied - not a team member or mentor.');
+    if (!(team.members.includes(req.user._id))) return res.status(403).send('Access denied - not a team member or mentor.');
 
     return res.status(200).send(team);
 });
 
 router.put('/:id', auth, async (req, res) => {
-    
-} )
+
+    if(!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send("Invalid object ID");
+    }
+
+    const team = await Team.findById(req.params.id);
+    if (!team) return res.status(400).send("Team does not exist.");
+    if (!(team.mentorId === req.user._id)) return res.status(403).send('Access denied - not the team mentor.');
+
+    if (req.query.name) {
+       team.name = req.query.name;
+       await Team.findByIdAndUpdate(team._id, team);
+       return res.status(200).send(team);
+    }
+
+    if (req.query.add) {
+
+        if (!ObjectId.isValid(req.query.add)) {
+            return res.status(400).send("Invalid object ID");
+        }
+
+        console.log(req.query.add);
+
+        const user = await User.findById(req.query.add);
+        if (!user) return res.status(400).send("User does not exist.");
+
+        if(team.members.includes(req.query.add)) return res.status(400).send("User already in team.");
+
+        team.members = [...team.members, req.query.add];
+        await Team.findByIdAndUpdate(team._id, team);
+        return res.status(200).send(team);
+    }
+
+    if (req.query.remove) {
+
+        if (!ObjectId.isValid(req.query.remove)) {
+            return res.status(400).send("Invalid object ID");
+        }
+
+        const user = await User.findById(req.query.remove);
+        if (!user) return res.status(400).send("User does not exist.");
+
+        if (!team.members.includes(req.query.remove)) return res.status(400).send("User not in team.");
+
+        team.members = team.members.filter((name) => name != req.query.remove);
+        await Team.findByIdAndUpdate(team._id, team);
+        return res.status(200).send(team);
+    }
+
+    const result = await Team.findById(req.params.id);
+    return res.status(200).send(result);
+
+});
 
 router.delete('/:id', auth, async (req, res) => {
-    const team = await Team.findById(req.params.id);
 
-    if (!(team.mentorId === req.body.mentorId)) return res.status(403).send('Access denied - not the team mentor.');
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send("Invalid object ID");
+    }
+
+    const team = await Team.findById(req.params.id);
+    if (!team) return res.status(400).send("Team does not exist.");
+    if (!(team.mentorId === req.user._id)) return res.status(403).send('Access denied - not the team mentor.');
 
     await Team.findByIdAndDelete(team._id);
 
