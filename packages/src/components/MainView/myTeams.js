2f5de974361838c8db00f7c1d7769c740a99bdb4
Kuba <ja.tanski@gmail.com>
2019-05-12 22:29:59 +0200

    change style myTeams

--- packages/src/components/MainView/myTeams.js
+++ packages/src/components/MainView/myTeams.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./myTeams.css";
-import {MyTeam} from './myTeam';
-
-
+import '../mainViewContainer/mainViewContainer.css';
+import '../mainViewContainer/checklistCounter.css';
+import '../mainViewContainer/checklistEditorContainer.css';
+import { MyTeam } from './myTeam';
 
 export default class MyTeams extends Component {
   state = {
+    teamsNumber: 2,
     data: [{ title: "Mój zespół 1", coworkers: ['kumpel@wp.pl', 'mail.@mail.pl', 'mail.@mail.pl', 'mail.@mail.pl', 'mail.@mail.pl', 'mail.@mail.pl'], checklisty: ['Fajna checklista'] },
-     {
+    {
       title: "Mój zespół 2",
       coworkers: []
     }
@@ -25,17 +27,23 @@ export default class MyTeams extends Component {
   //     .catch(err => console.log(err))
   // }
   render() {
+    const windowWidth = window.innerWidth;
+
     return (
-      <section id="myteams">
-        <div className="myteams_content">
-            <div className="myteams_buttons">
-              <button className="countTeamButton">2 Liczba teamów</button>
-            </div>
-            <div className="myteam">
-              <MyTeam data={this.state.data[0].coworkers} title={this.state.data[0].title} checklisty={this.state.data[0].checklisty} />
-            </div>
+      <section className="mainView__container">
+        <div className="mainView__header">
+          {windowWidth < 1025 ? <div onClick={this.props.onClick}><i className="fas fa-bars"></i></div> : null}
+          <h2>Panel mentora</h2>
         </div>
-        <div className="myteams_container">
+        <div className="stateContainter ">
+          <button className="state checklistCounter"><span className="checklistNumber">{this.state.teamsNumber}</span>
+            <p>Liczba zespołów</p></button>
+        </div>
+        <div className="myteam">
+          <MyTeam data={this.state.data[0].coworkers} title={this.state.data[0].title} checklisty={this.state.data[0].checklisty} />
+        </div>
+
+        <div className="checklistEditor__container">
           <div className="myteams_title">
             <i className="material-icons">
               people
