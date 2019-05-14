7083edd5ed28eff6d9d9d3b5eddcbcda611b07e2
Gustaw <tanski.gustaw@gmail.com>
2019-05-14 21:57:59 +0200

    Theme is saved in localhost

--- packages/src/components/App/App.js
+++ packages/src/components/App/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import './App.css';
-import Login from '../Login/login';
-import Logout from "../Logout"
-import Register from "../Register/register"
-import SplashScreen from '../SplashScreen/splashScreen';
-import Error404 from "../Error404"
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom'
-import MainView from '../MainView/mainView';
+import React from "react";
+import "./App.css";
+import Login from "../Login/login";
+import Logout from "../Logout";
+import Register from "../Register/register";
+import SplashScreen from "../SplashScreen/splashScreen";
+import Error404 from "../Error404";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainView from "../MainView/mainView";
 
+const Routes = (
+	<Switch className="App">
+		<Route path="/me/myteams" component={MainView} />
+		<Route path="/me" component={MainView} />
+		<Route path="/login" component={Login} />
+		<Route path="/register" component={Register} />
+		<Route path="/logout" component={Logout} />
+		<Route exact path="/" component={SplashScreen} />
+		<Route path="/" component={Error404} />
+	</Switch>
+);
 
 const App = () => {
-    return (
-        <Router>
-            <Switch className="App">
-                <Route path="/me/myteams" component={MainView} />
-                <Route path='/me' component={MainView} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/logout" component={Logout} />
-                <Route exact path="/" component={SplashScreen} />
-                <Route path="/" component={Error404} />
-            </Switch>
-        </Router>
-    );
+	const theme = localStorage.getItem("theme");
+	if (theme) document.documentElement.setAttribute("data-theme", theme);
+	return <Router>{Routes}</Router>;
 };
 
 export default App;
--- packages/src/components/App/ThemeChanger.js
+++ packages/src/components/App/ThemeChanger.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class ThemeChanger extends Component {
-    state = {
-        theme: 'light',
-    };
+	state = {
+		theme: localStorage.getItem("theme")
+	};
 
+	toggleTheme = () => {
+		const theme = this.state.theme === "dark" ? "light" : "dark";
+		this.setState({ theme });
+		document.documentElement.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
+	};
 
-toggleTheme = () => {
-    const theme = this.state.theme === 'dark' ? 'light' : 'dark';
-    this.setState({ theme });
-    document.documentElement.setAttribute("data-theme", theme);
-}
-
-render() {
-    return <span onClick={() => this.toggleTheme()}>
-    Zmień motyw
-    </span> 
-};
+	render() {
+		return <span onClick={() => this.toggleTheme()}>Zmień motyw</span>;
+	}
 }
